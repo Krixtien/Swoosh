97edbcc2eecc7f37f35a8683646618bb4b408607
krixtien <comfirm247@gmail.com>
2018-11-19 12:00:55 +0100

    lesson 10 -> savedInstanceState working Perfectly

--- app/src/main/java/com/example/nwaka/swoosh/Controller/SkillActivity.kt
+++ app/src/main/java/com/example/nwaka/swoosh/Controller/SkillActivity.kt
@@ -1,21 +1,34 @@
 package com.example.nwaka.swoosh.Controller
 import android.content.Intent
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.view.View
 import android.widget.Toast
 import com.example.nwaka.swoosh.Model.Player
 import com.example.nwaka.swoosh.R
 import com.example.nwaka.swoosh.Utilities.EXTRA_PLAYER
 import kotlinx.android.synthetic.main.activity_skill.*
-
 class SkillActivity : BaseActivity() {
 
     lateinit var player: Player
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        outState?.putParcelable(EXTRA_PLAYER, player)
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_skill)
         player = intent.getParcelableExtra(EXTRA_PLAYER)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if (savedInstanceState != null){
+            player = savedInstanceState.getParcelable(EXTRA_PLAYER)
+        }
     }
 
     fun onBallerClick(view: View){
